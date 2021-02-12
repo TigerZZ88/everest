8fe97d7e7ebedeca3e6768c5d5090b26ac1d2146
Aseem Rastogi <aseemr@microsoft.com>
2021-02-12 11:57:37 +0530

    update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=97132381e01f6277465aea5e970d152573e998e8
-branches[FStar]=master
+hashes[FStar]=2adf669cc6d87c1f877aa8934f841b669da891df
+branches[FStar]=aseem_nik_matbuj_attrs
 hashes[hacl-star]=efd58de2c02a0050a6d47b9603a273b8f9b28e56
 branches[hacl-star]=master
 hashes[kremlin]=7f4f6c11f9b3910a10f5856286d46543fd8e535c
@@ -10,5 +10,5 @@ hashes[mitls-fstar]=1585d8d3ac0afa49594770ef82f8a55db51b222c
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=5a2f68c1363512b33d0a344c196b0104ae326388
-branches[quackyducky]=master
+hashes[quackyducky]=40c475ca64661d4e9e56d787123a3326b5c4316d
+branches[quackyducky]=aseem_load_cmxs
